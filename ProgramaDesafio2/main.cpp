8bd60fcba724c8d30e17ef01af37298846616430
Sebastian Osorio <sebastian.osorio15@udea.edu.co>
2024-10-13 17:32:22 -0500

    Se agregaron unas lineas para prueba

--- ProgramaDesafio2/main.cpp
+++ ProgramaDesafio2/main.cpp
@@ -4,28 +4,23 @@
 #include "clasesPrograma.h"
 using namespace std;
 int main(){
+    srand (time(NULL));
 
-    //Surtidor surtudor(104,1234);
-    string region;
-    int com1, com2, com3;
-    cout << "Para que region deceas actualizar los precios: ";
-    cin >> region;
-    cout << "Precio para regular: ";
-    cin >> com1;
-    cout << "Precio para Premium: ";
-    cin >> com2;
-    cout << "Precio para ecoExtra: ";
-    cin >> com3;
-
-    TanqueCentral h(101,400);
+    /*TanqueCentral h(101,400);
     h.setC_ecoExtra(0,com3);
     h.setC_regular(0, com1);
     h.setC_premium(0, com2);
-    h.fijarPreciosCom();
+    h.fijarPreciosCom();*/
+    Surtidor surtidor(567,12345);
+    surtidor.mostrarHistorial();
 
     //surtudor.mostrarHistorial();
-    cout << "Hello World!" << endl;
-    //leer_registro_transaccion("archivoRegistros.txt");
+    //cout << "Hello World!" << endl;
+    //leer_registro_transaccion("archivoRegistros.txt");*/
+    //TanqueCentral tanqueCentral;
+    //tanqueCentral.fijarPreciosCom();
+    //tanqueCentral.fijarPreciosCom();
+    //surtidor.eliminarSurtidor();
     cout << "hola";
     return 0;
 }
