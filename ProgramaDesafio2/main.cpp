fc672b3337d2c1ccb118089d18c70e8fd9dff5b3
Sebastian Osorio <sebastian.osorio15@udea.edu.co>
2024-10-14 22:03:23 -0500

    Se agrego el menú, faltan algunas funcionalidades

--- ProgramaDesafio2/main.cpp
+++ ProgramaDesafio2/main.cpp
@@ -2,29 +2,93 @@
 #include <fstream>
 #include <string>
 #include "clasesPrograma.h"
+void menu();
 using namespace std;
 int main(){
-    srand (time(NULL));
 
-    /*TanqueCentral h(101,400);
-    h.setC_ecoExtra(0,com3);
-    h.setC_regular(0, com1);
-    h.setC_premium(0, com2);
-    h.fijarPreciosCom();*/
-    //Surtidor surtidor(567,12345);
-    //surtidor.mostrarHistorial();
-    TanqueCentral tanqueCentral;
-    tanqueCentral.leerCodigosEstacion();
-    TanqueCentral _tanqueCentral(tanqueCentral.getCantidadE());
+    Estacion estacion;
+    TanqueCentral tanquecentral;
+    Surtidor surtidor;
+    Transaccion transaccion;
 
-    //surtudor.mostrarHistorial();
-    //cout << "Hello World!" << endl;
-    //leer_registro_transaccion("archivoRegistros.txt");*/
-    //TanqueCentral tanqueCentral;
-    //tanqueCentral.fijarPreciosCom();
-    //tanqueCentral.fijarPreciosCom();
-    //surtidor.eliminarSurtidor();
-    cout << "hola";
+    string opcion;
+
+    while(true){
+        menu();
+        cout << "Que accion deceas realizar?: ";
+        cin >> opcion;
+        if (opcion == "a"){
+
+            estacion.agregarEstacion();
+
+        }else if (opcion == "b"){
+
+            estacion.eliminarEstacion();
+
+        }else if (opcion == "c"){//falta
+
+        }else if (opcion == "d"){//Falta
+
+        }else if (opcion == "1"){
+
+            surtidor.agregarSurtidor();
+
+        }else if (opcion == "2"){
+
+            surtidor.eliminarSurtidor();
+
+        }else if (opcion == "3"){
+
+            surtidor.modificarAc();
+
+        }else if (opcion == "4"){
+
+            surtidor.modificarIn();
+
+        }else if (opcion == "5"){
+
+            surtidor.mostrarHistorial();
+
+        }else if (opcion == "6"){//Falta
+
+        }else if (opcion == "z"){
+
+            transaccion.simularVenta();
+
+        }else if (opcion == "w"){
+
+            tanquecentral.verificarFugas();
+
+        }else if (opcion == "0"){
+
+            cout << "Hasta pronto!" << endl;
+            break;
+
+        }else{
+            cout << "La accion ------> " << opcion << " No fue encontrada..." << endl;
+        }
+    }
+
+
     return 0;
 }
 
+void menu(){
+
+    cout << " -------------------------------------------------------" << endl;
+    cout << "|                        TerMax                         |" << endl;
+    cout << " -------------------------------------------------------" << endl;
+    cout << "|  Gestion de la red      |     Gestion estacion        |" << endl;
+    cout << " -------------------------------------------------------" << endl;
+    cout << "| a. Agregar estacion     |  1. Agregar surtidor        |" << endl;
+    cout << "| b. ELiminar estacion    |  2. Eliminar surtidor       |" << endl;
+    cout << "| c. Calcular ventas      |  3. Activar surtidor        |" << endl;
+    cout << "| d. Fijar precios com-   |  4. Desactivar surtidor     |" << endl;
+    cout << "|    bustible             |  5. Historico transacciones |" << endl;
+    cout << "|                         |  6. Cantidad litros vendida |" << endl;
+    cout << " -------------------------------------------------------" << endl;
+    cout << "| z. Simular venta           w. Verificar fugas         |" << endl;
+    cout << "|               | 0. Cerrar programa |                  |" << endl;
+    cout << " -------------------------------------------------------" << endl;
+
+}
