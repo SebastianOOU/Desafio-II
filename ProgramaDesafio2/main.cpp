a3156a2eee2a01057ee4e78b9bc85194ce91df30
Sebastian Osorio <sebastian.osorio15@udea.edu.co>
2024-10-17 17:20:07 -0500

    Se ha modificado el menu

--- ProgramaDesafio2/main.cpp
+++ ProgramaDesafio2/main.cpp
@@ -5,23 +5,26 @@
 using namespace std;
 
 void menu();
+void simularVentaEstacion();
 void pedirDatosEstacion(string &, string &, string &, string &, string &);
 void pedirDatosSurtidor(string &, string &, string &);
 
 int main(){
-
+    srand (time(NULL)); //Para la funcion rand()
+    TanqueCentral tanqueCentral;
     char opcion;
     string codigo, modeloS, ubicacion;
     string nombreE, gerenteE, regionE;
 
     while(true){
-        menu();
+
+        menu();//Mostrar menú
         cout << "Que accion deceas realizar?: ";
         cin >> opcion;
+
         if (opcion == 'a'){
-
+            //Agregar una nueva estacion
             pedirDatosEstacion(nombreE,codigo,gerenteE,regionE,ubicacion);
-
             Estacion estacion(nombreE,codigo,gerenteE,regionE,ubicacion);
             estacion.agregarEstacion();
 
@@ -29,85 +32,113 @@ int main(){
 
             //estacion.eliminarEstacion();
 
-        }else if (opcion == 'c'){//falta
+        }else if (opcion == 'c'){
+            //Cantidad COP nivel nacional
+            Estacion estacion("none");
+            estacion.calcularMonto(true);//True datos Nacionales, 4 verificar solo precios, false verificar fugas
 
-            Estacion estacion("0", "120","0","0","0");
-            estacion.calcularMonto(true);
+        }else if (opcion == 'd'){
+            //Cantidad ltrs nivel nacional
+            Estacion estacion("none");
+            estacion.reportarCanLitros(true);//True datos Nacionales, 3 verificar solo listros, false verificar fugas
 
-        }else if (opcion == 'd'){//Falta
-
-            Estacion estacion("0", "120","0","0","0");
-            estacion.reportarCanLitros(true);
+        }else if (opcion == 'e'){
+            //Precios nuevos para cada region (Sur,Norte,centro)
+            tanqueCentral.fijarPreciosCom();
 
         }else if (opcion == '1'){
-
+            //Agregar un nuevo surtidor;
             pedirDatosSurtidor(codigo,modeloS,ubicacion);
-
             Surtidor surtidor(codigo,modeloS,ubicacion);
             surtidor.setEstado("activo");
             surtidor.agregarSurtidor();
-            cout << "Surtidor agregado correctamente";
 
         }else if (opcion == '2'){
-
+            //Eliminar surtidor (solo si esta inactivo)
             pedirDatosSurtidor(codigo,modeloS,ubicacion);
-
             Surtidor surtidor(codigo,modeloS,ubicacion);
             surtidor.setEstado("inactivo");
             surtidor.eliminarSurtidor();
 
-            cout << "Surtidor eliminado..." << endl;
-
         }else if (opcion == '3'){
-
+            //Activar surtidor (solo si esta inactivo)
             pedirDatosSurtidor(codigo,modeloS,ubicacion);
-
             Surtidor surtidor(codigo,modeloS,ubicacion);
             surtidor.setEstado("inactivo");
             surtidor.modificarAcIn(1);
 
-            cout << "Surtidor activado..." << endl;
-
         }else if (opcion == '4'){
-
+            //Desactivar surtidor (solo si esta incativo)
             pedirDatosSurtidor(codigo,modeloS,ubicacion);
-
             Surtidor surtidor(codigo,modeloS,ubicacion);
             surtidor.setEstado("activo");
             surtidor.modificarAcIn(0);
 
-            cout << "Surtidor desactivado..." << endl;
-
         }else if (opcion == '5'){
-
+            //Muestra el historial del surtidor en un archivo.txt
             cout << "Ingrese el codigo de la estacion: ";
             cin >> codigo;
             cout << "Ingrese el modelo del surtidor: ";
             cin >> modeloS;
-
             Surtidor surtidor(codigo,modeloS);
             surtidor.mostrarHistorial();
 
         }else if (opcion == '6'){
-
-            Estacion estacion("0", "120","0","0","0");
-            estacion.reportarCanLitros(false);
+            //Cantidad ltrs de cada estacion pedida
+            cout << "Ingrese el codigo de la estacion: ";
+            cin >> codigo;
+            Estacion estacion(codigo);
+            estacion.reportarCanLitros(false);//Falso datos Nacionales, 3 verificar solo precios, false para verificar fugas
 
         }else if (opcion == '7'){
+            //cantidad COP de cada estacion pedida
+            cout << "Ingrese el codigo de la estacion: ";
+            cin >> codigo;
+            Estacion estacion(codigo);
+            estacion.calcularMonto(false);//Falso datos Nacionales, 4 verificar solo precios, false para verificar fugas
 
-            Estacion estacion("0", "120","0","0","0");
-            estacion.calcularMonto(false);
+        }else if (opcion == '8'){
+            //Se asigna un numero aleatorio para el combustible
+            cout << "Ingrese el codigo de la estacion: ";
+            cin >> codigo;
+            TanqueCentral tanquecentral(codigo);
+            tanquecentral.asignarcantLitros();
 
         }else if (opcion == 'z'){
+            //Se simula una venta de combustible
+            int a, b;
+            string codigo, categoria, metodoPa, nombre, documento;
 
-           // transaccion.simularVenta();
-            Transaccion transaccion(120,23,20,"ecoExtra",23000,"debito");
-            transaccion.setDat_cliente("Sebas", "12345566");
-            transaccion.mostrarRegistroventa();
+            cout << "Ingrese el codigo de la estacion: ";
+            cin >> codigo;
+            cout << "Que combustible quieres usar (Regular, Premium, ecoExtra): ";
+            cin >> categoria;
+            cout << "Ingrese su metodo de pago: ";
+            cin >> metodoPa;
+            cout << "Ingrese su nombre: ";
+            cin >> nombre;
+            cout << "Ingrese su documento: ";
+            cin >> documento;
+
+            Transaccion transaccion(codigo,categoria,metodoPa);
+            transaccion.setDat_cliente(nombre, documento);
+
+            transaccion.verificarEstacion(a,b);
+
+            if(a == 1){
+                transaccion.simularVenta(tanqueCentral.getPrecioSur1(),tanqueCentral.getPrecioSur2(),tanqueCentral.getPrecioSur3(),b);
+            }else if(a == 2){
+                transaccion.simularVenta(tanqueCentral.getPrecioNorte1(),tanqueCentral.getPrecioNorte2(),tanqueCentral.getPrecioNorte3(),b);
+            }else{
+                transaccion.simularVenta(tanqueCentral.getPrecioCentro1(),tanqueCentral.getPrecioCentro2(),tanqueCentral.getPrecioCentro3(),b);
+            }
 
         }else if (opcion == 'w'){
-
-         //   tanquecentral.verificarFugas();
+            //Se revisa si hay posibles fugas
+            cout << "Ingrese el codigo de la estacion: ";
+            cin >> codigo;
+            Estacion estacion(codigo);
+            estacion.calcularMonto(false,3,true);//Falso datos Nacionales,3 verificar solo litros, true para verificar fugas
 
         }else if (opcion == '0'){
 
@@ -118,8 +149,6 @@ int main(){
             cout << "La accion ------> " << opcion << " No fue encontrada..." << endl;
         }
     }
-
-
     return 0;
 }
 
@@ -137,6 +166,7 @@ void menu(){
     cout << "|  e. Fijar precios com-       |    5. Historico transacciones    |" << endl;
     cout << "|     bustible                 |    6. Cantidad litros vendida    |" << endl;
     cout << "|                              |    7. Calcular Ventas            |" << endl;
+    cout << "|                              |    8. Asignar cantidad de litros |" << endl;
     cout << " -----------------------------------------------------------------" << endl;
     cout << "|  z. Simular venta              w. Verificar fugas               |" << endl;
     cout << "|                 - 0. Cerrar programa -                          |" << endl;
@@ -162,8 +192,10 @@ void pedirDatosEstacion(string &_nombre, string &_codigo, string &_gerente, stri
     cin >> _codigo;
     cout << "Ingrese el nombre del gerente: ";
     cin >> _gerente;
-    cout << "Ingrese el nombred e la region: ";
+    cout << "Ingrese el nombre de la region: ";
     cin >> _region;
     cout << "Ingrese la ubicacion de la estacion: ";
     cin >> _ubicacion;
 }
+
+
